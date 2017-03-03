baddfb6dbf8b9ca511453ffd54c62e59d0652c07
Taewoo Kim <taewookim03@gmail.com>
2017-03-02 18:39:02 -0600

    First commit

---
+++ api-sample-call/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(){
+	document.getElementById("submit").addEventListener("click", function(event){
+		var req = new XMLHttpRequest();
+		var imageURL = document.getElementById("imgURL").value;
+		//var imageURL = encodeURIComponent(document.getElementById("imgURL").value);
+
+		//https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Description,Tags&subscription-key=<Your subscription key>
+		var reqURL = "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?";
+		var apikey = "5fe3911888324f3a93c349d028981387";
+		var visualFeatures = document.getElementById("visualFeatures").value;//Categories, Tags, Description, Faces, ImageType, Color, Adult
+		req.open("POST", reqURL + "visualFeatures=" + visualFeatures + "&subscription-key=" + apikey, true);
+		req.setRequestHeader("Content-Type", "application/json");
+		var payload = { "url" : imageURL };
+		
+		req.addEventListener("load", function(){
+			if (req.status >= 200 && req.status < 400) {
+				//var response = JSON.parse(req.responseText);
+				document.getElementById("response").textContent = req.responseText;
+			} 
+			else {
+				console.log("Error in network request" + req.statusText);
+			}
+		});
+	req.send(JSON.stringify(payload));
+	event.preventDefault();
+	});
+});
